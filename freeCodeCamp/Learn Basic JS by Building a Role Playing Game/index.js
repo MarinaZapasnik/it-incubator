a0f92ee6de15f5d3637e0e870378c8b59221b0d8
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-03-13 12:52:01 +0300

    free code camp, stepd 42-44

--- freeCodeCamp/Learn Basic JS by Building a Role Playing Game/index.js
+++ freeCodeCamp/Learn Basic JS by Building a Role Playing Game/index.js
@@ -23,13 +23,18 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function goStore() {
-  console.log("Going to store.");
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+   // button1.onclick = buyHealth;
+    //button2.onclick = buyWeapon;
+    //button3.onclick = goTown;
 }
 
 function goCave() {
-  console.log("Going to cave.");
+    console.log("Going to cave.");
 }
 
 function fightDragon() {
-  console.log("Fighting dragon.");
+    console.log("Fighting dragon.");
 }
